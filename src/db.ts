443a2b5a3ff9eabc29c153c88c1eda0d4bb3160e
Henok Yoseph <henok.y2014@gmail.com>
2025-12-27 10:21:21 +0300

    Change db path for prod deployment

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,8 @@
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+// Dev
+// const sqlite = new Database("db.sqlite");
+// Prod
+const sqlite = new Database("/data/db.sqlite");
 
-const sqlite = new Database("db.sqlite");
 export const db = drizzle(sqlite);
