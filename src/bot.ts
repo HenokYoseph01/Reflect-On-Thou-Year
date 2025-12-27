fb19050c0ab2df6b94d036a8ec40d1e9c7b63bfd
Henok Yoseph <henok.y2014@gmail.com>
2025-12-27 10:11:43 +0300

    fix: Determine bot token exists

--- src/bot.ts
+++ src/bot.ts
@@ -4,4 +4,4 @@ if (!process.env.BOT_TOKEN) {
   throw new Error("BOT_TOKEN is missing");
 }
 
-export const bot = new Bot(process.env.BOT_TOKEN);
+export const bot = new Bot(process.env.BOT_TOKEN!);
